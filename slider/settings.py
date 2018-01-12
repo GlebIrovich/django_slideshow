8975dbfddb0af04e5e771c945db47bd62137366c
Cookie Hunter <onlyspamhier@gmail.com>
2018-01-12 16:34:01 +0100

    Webfaction deployment

--- slider/settings.py
+++ slider/settings.py
@@ -41,7 +41,7 @@ SECRET_KEY = '99j*4yjjo=7!4vd4wqiumc-&7m=9vw1jc4+h0(h^#9u&+a+iyy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0', 'uzh.cookiehunter@webfactional.com']
 
