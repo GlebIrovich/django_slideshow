77430c35998940ca60a09de6ed9c442768b726ed
Cookie Hunter <onlyspamhier@gmail.com>
2018-01-13 09:12:10 +0100

    Webfaction deployment serving media

--- slider/urls.py
+++ slider/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     url(r'^', include('carousel.urls')),
    
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
 
 urlpatterns += [
     url(r'^accounts/', include('django.contrib.auth.urls')),
